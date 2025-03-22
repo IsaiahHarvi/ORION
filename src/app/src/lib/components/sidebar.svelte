47c1c55c9bee783d985ba7ff0b3c1a0b36b05965
Aidan Neel <aaidan5899@gmail.com>
2025-03-22 17:52:36 -0500

    Mobile support

--- src/app/src/lib/components/sidebar.svelte
+++ src/app/src/lib/components/sidebar.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-    import { Button } from '$lib/components/ui/button'
+    import { Button, buttonVariants } from '$lib/components/ui/button'
     import { page } from '$app/stores';
 	import type { Snippet } from 'svelte';
+    import * as Sheet from "$lib/components/ui/sheet";
     import Globe from '@lucide/svelte/icons/globe'
+    import Menu from '@lucide/svelte/icons/menu'
 
     let { children }: {
         children?: Snippet
@@ -10,7 +12,7 @@
 
     const tabs = [
         {
-            name: 'Radar',
+            name: 'RADAR',
             href: '/',
         },
         {
@@ -19,8 +21,8 @@
         }
     ];
 </script>
- 
-<div class="bg-neutral-900 flex-shrink-0 border-r invisible md:visible md:w-64 lg:w-80 px-4 h-screen flex flex-col">
+
+{#snippet sidebarContent()}
     <h1 class='text-foreground flex flex-row items-center gap-2 text-lg pt-4 font-medium'>
         <Globe size={20} />
         ORION
@@ -33,4 +35,23 @@
             </Button>
         {/each}
     </div>
+{/snippet}
+ 
+<div class="bg-neutral-900 flex-shrink-0 border-r invisible lg:visible lg:w-64 xl:w-[20rem] px-4 h-screen flex flex-col">
+    {@render sidebarContent()}
+</div>
+
+<div class="bg-neutral-900 border-b px-4 gap-4 z-40 absolute flex items-center flex-row h-16 top-0 left-0 w-screen visible md:invisible">
+    <Sheet.Root>
+        <Sheet.Trigger class={buttonVariants({ variant: "outline", size: 'icon' })}>
+            <Menu size={16} />
+        </Sheet.Trigger>
+        <Sheet.Content side="left">
+            {@render sidebarContent()}
+        </Sheet.Content>
+    </Sheet.Root>
+    <h1 class='text-foreground flex flex-row items-center gap-2 text-lg font-medium'>
+        <Globe size={20} />
+        ORION
+    </h1>
 </div>
