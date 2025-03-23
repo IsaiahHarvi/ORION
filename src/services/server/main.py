26b65a89d67a01b3b5b4eee3f63cbcd382877160
isaiahharvi <harvillerisaiah@gmail.com>
2025-03-23 06:02:58 +0000

    delete routers thing and server on all

--- src/services/server/main.py
+++ src/services/server/main.py
@@ -2,7 +2,6 @@ import os
 
 import pandas as pd
 from fastapi import FastAPI
-from routers import cursor
 
 from services.scans.get_scans import download_scans
 from services.scans.get_stations import get_nearby_radars
@@ -15,7 +14,6 @@ model_ = Model()
 DATA_DIR = os.environ.get("SCAN_DIR", "./data/scans")
 os.makedirs(os.path.join(DATA_DIR, "extract"), exist_ok=True)
 
-app.include_router(cursor.router)
 
 @app.get("/")
 async def root():
@@ -27,62 +25,6 @@ async def model(lat, lon):
     if not len(radars):
         return {"Error" : "Could not find radars"}, 500
     return radars
-
-@app.get("/model/{lat}/{lon}/{timestamp}")
-async def model(lat, lon, timestamp: str):
-    radars = get_nearby_radars(float(lat), float(lon), radius_km=200)
-    if not len(radars):
-        return {"Error" : "No radars in 200km range"}, 500
-    else:
-        nearest_radar = radars[0]
-        nearest_radar = "KDVN"
-
-    timestamp = pd.Timestamp(*map(int, timestamp.split("-"))).tz_localize("UTC")
-    scans = download_scans(nearest_radar, timestamp, DATA_DIR, scan_count=1)
-    if not scans.success_count:
-        return {"Error" : f"{nearest_radar} has 0 scans currently available."}, 500
-
-    print(os.listdir(DATA_DIR))
-    files = [os.path.join(DATA_DIR, f) for f in os.listdir(DATA_DIR)
-            if os.path.isfile(os.path.join(DATA_DIR, f))]
-    latest_scan = max(files, key=os.path.getmtime)
-    print(files)
-
-    enforce_dir_size_limit(DATA_DIR, max_size_bytes=3*1024*1024*1024)
-
-    print(f"Sending {latest_scan} to model")
-
-    tornado_probability = model_.predict(latest_scan)
-
-    return {"probability" : tornado_probability}
-
-@app.get("/model/{lat}/{lon}/{timestamp}")
-async def model(lat, lon, timestamp: str):
-    radars = get_nearby_radars(float(lat), float(lon), radius_km=200)
-    if not len(radars):
-        return {"Error" : "No radars in 200km range"}, 500
-    else:
-        nearest_radar = radars[0]
-        nearest_radar = "KDVN"
-
-    timestamp = pd.Timestamp(*map(int, timestamp.split("-"))).tz_localize("UTC")
-    scans = download_scans(nearest_radar, timestamp, DATA_DIR, scan_count=1)
-    if not scans.success_count:
-        return {"Error" : f"{nearest_radar} has 0 scans currently available."}, 500
-
-    print(os.listdir(DATA_DIR))
-    files = [os.path.join(DATA_DIR, f) for f in os.listdir(DATA_DIR)
-            if os.path.isfile(os.path.join(DATA_DIR, f))]
-    latest_scan = max(files, key=os.path.getmtime)
-    print(files)
-
-    enforce_dir_size_limit(DATA_DIR, max_size_bytes=3*1024*1024*1024)
-
-    print(f"Sending {latest_scan} to model")
-
-    tornado_probability = model_.predict(latest_scan)
-
-    return {"probability" : tornado_probability}
 
 @app.get("/model/{lat}/{lon}/{timestamp}")
 async def model(lat, lon, timestamp: str):
