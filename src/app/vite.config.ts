7bb018951a75b819e1b7d6c765631a70462bc79c
isaiahharvi <harvillerisaiah@gmail.com>
2025-03-24 19:07:52 +0000

    fix hot reloading, editorconfigs, reqs

--- src/app/vite.config.ts
+++ src/app/vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		allowedHosts: ['orion.harville.dev'],
+		allowedHosts: ['orion.harville.dev', 'localhost', '127.0.0.1'],
+		host: '0.0.0.0',
+		port: 5173,
+		watch: {
+			usePolling: true
+		}
 	}
 });
