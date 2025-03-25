87e2e21d2bc35d7faf35631a8f7e1ea72a8f8f4e
Isaiah Harville <iharville@trideum.com>
2025-03-25 07:52:18 -0500

    test out using same domain diff location

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 docker compose --profile gui down -v || true
 
-export VITE_API_URL=https://orion-api.harville.dev
+export VITE_API_URL=https://orion.harville.dev/api
 
 docker compose --profile gui up --build -d
